3caddbda0a7cd176c9e59fede3f66011573800ec
mikebertiean <mbertiean@hotmail.com>
2013-11-14 13:07:41 -0500

    added peripherals package

---
+++ sidescroller/sidescroller/src/main/java/com/example/sidescroller/peripherals/Bullet.java
@@ -0,0 +1,9 @@
+package com.example.sidescroller.peripherals;
+
+import android.graphics.Point;
+
+public class Bullet {
+    //img file
+    private int speed;
+    private Point start, end;
+}
---
+++ sidescroller/sidescroller/src/main/java/com/example/sidescroller/peripherals/Weapon.java
@@ -0,0 +1,11 @@
+package com.example.sidescroller.peripherals;
+
+/**
+ * Created by Owner on 14/11/13.
+ */
+public class Weapon {
+    private String name;
+    private int damage, rof; //ROF = rate of fire
+    //image file
+    private Bullet bullet;
+}
